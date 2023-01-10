f7e146e6f70d085d0a7833f2df3d009657d486e2
emily <75251243+shyiyan@users.noreply.github.com>
2023-01-11 00:40:18 +0800

    empty home screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate, BrowserRouter } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import axios from "axios";
@@ -6,17 +7,8 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState("");
-  const getData = async () => {
-    const res = await axios.get("http://localhost:3000/profile");
-    console.log("hi");
-    console.log(res);
-    setMessage(res.data.message);
-  };
-  useEffect(() => {
-    getData();
-  }, []);
   return (
+    <>
     <ButtonGroup
       style={{
         display: "flex",
@@ -28,8 +20,10 @@ function App() {
       <Button className="button-23">Resume</Button>
       <Button className="button-23">My Favorite Things</Button>
       <Button className="button-23">Blog</Button>
-      //<Button className="button-23">Contact Me</Button>
+      <Button className="button-23">Contact Me</Button>
     </ButtonGroup>
+    <BrowserRouter></BrowserRouter>
+    </>
   );
 }
 
---
+++ client/src/HomeScreen.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, BrowserRouter } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+
+function HomeScreen() {
+  const [message, setMessage] = useState("");
+  //const navigate = useNavigate();
+  const getData = async () => {
+    const res = await axios.get("http://localhost:3000/profile");
+    setMessage(res.data.message);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <>
+    </>
+  );
+}
+
+export default HomeScreen;
